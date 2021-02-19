2ae7923d56f60bf66979e361ed338897d3e85ece
Kartik Mehetre <kartik.mehetre17@vit.edu>
2021-02-19 17:42:29 +0530

    Committing code for ref

---
+++ client/src/components/election/electionComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Election extends Component {
+  render() {
+    return (
+     <div>
+         <h1>
+             HeLLOOO
+         </h1>
+     </div>
+    );
+  }
+}
+
+export default Election;
